4d894aa19db5c847378b1cfda104f211697175e9
ChrisHilborne <chris.hilborne@gmail.com>
2021-01-06 13:44:14 +0100

    First Commit: Application Bootstrapped and Dependencies Imported Using  Spring Initzr

---
+++ src/main/java/com/chilborne/covidradar/CovidRadarApplication.java
@@ -0,0 +1,13 @@
+package com.chilborne.covidradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chilborne/covidradar/CovidRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilborne.covidradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
