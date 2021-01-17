ce48a0b8b2a459b8e78944d4b4be8981ffad5a0a
ChrisHilborne <chris.hilborne@gmail.com>
2021-01-17 17:25:28 +0100

    Finished unit tests for DistrictDataServiceImpl()

--- src/test/java/com/chilborne/covidradar/services/DistrictDataServiceImplTest.java
+++ src/test/java/com/chilborne/covidradar/services/DistrictDataServiceImplTest.java
@@ -4,53 +4,96 @@ import com.chilborne.covidradar.model.DailyRecord;
 import com.chilborne.covidradar.model.DistrictData;
 import com.chilborne.covidradar.model.DistrictDataDTO;
 import com.chilborne.covidradar.repository.DistrictDataRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class DistrictDataServiceImplTest {
-
-
-    DistrictDataServiceImpl districtDataService;
 
     @Mock
     DistrictDataRepository districtDataRepository;
 
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.openMocks(this);
-        districtDataService = new DistrictDataServiceImpl(districtDataRepository);
-    }
+    @InjectMocks
+    DistrictDataServiceImpl districtDataService;
+
+    @Captor
+    ArgumentCaptor<DistrictData> districtDataCaptor;
 
     @Test
     void getDistrictDataByNameException() {
-        Exception e = assertThrows(RuntimeException.class, () -> {
-            districtDataService.getDistrictDataByName("test");
+        //given
+
+        //when
+        when(districtDataRepository.findByName("name")).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(RuntimeException.class, () -> {
+            districtDataService.getDistrictDataByName("name");
         });
+        verify(districtDataRepository, times(1)).findByName("name");
+
     }
 
     @Test
     void getDistrictDataByName() {
+        //given
+        DistrictData testDistrictData = new DistrictData();
+        testDistrictData.setName("one");
+        testDistrictData.setDailyRecords(new ArrayList<DailyRecord>());
+
+        //when
+        when(districtDataRepository.findByName("one")).thenReturn(Optional.of(testDistrictData));
+        DistrictDataDTO result = districtDataService.getDistrictDataByName("one");
+
+        //then
+        verify(districtDataRepository, times(1)).findByName("one");
+        assertEquals("one", result.getMunicipalDistrict());
         
     }
 
     @Test
     void getDistrictDataByGeoCode() {
+        //given
+        DistrictData testDistrictData = new DistrictData();
+        testDistrictData.setGeoCode("geoCode");
+        testDistrictData.setDailyRecords(new ArrayList<DailyRecord>());
+
+        //when
+        when(districtDataRepository.findByGeoCode("geoCode")).thenReturn(Optional.of(testDistrictData));
+        DistrictDataDTO result = districtDataService.getDistrictDataByGeoCode("geoCode");
+
+        //then
+        verify(districtDataRepository, times(1)).findByGeoCode("geoCode");
+        assertEquals("geoCode", result.getGeoCode());
+
     }
 
     @Test
     void getDistrictDataByGeoCodeException() {
-        Exception e = assertThrows(RuntimeException.class, () -> {
-            districtDataService.getDistrictDataByGeoCode("test");
+
+        //given
+
+        //when
+        when(districtDataRepository.findByGeoCode("geoCode")).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(RuntimeException.class, () -> {
+            districtDataService.getDistrictDataByGeoCode("geoCode");
         });
+        verify(districtDataRepository, times(1)).findByGeoCode("geoCode");
     }
 
     @Test
@@ -93,6 +136,18 @@ class DistrictDataServiceImplTest {
 
     @Test
     void save() {
+        //given
+        DistrictData toSave = new DistrictData();
+        toSave.setName("toSave");
+
+        //when
+        districtDataService.save(toSave);
+
+        //then
+        verify(districtDataRepository, times(1)).save(toSave);
+        verify(districtDataRepository).save(districtDataCaptor.capture());
+        assertEquals("toSave", districtDataCaptor.getValue().getName());
+        
     }
 
 
