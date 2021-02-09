8614f4f51af0eecfc07ae3a2bcfb8a59fb3c004e
ChrisHilborne <chris.hilborne@gmail.com>
2021-02-09 17:18:32 +0100

    removed CORs restrictions to ease development

--- src/main/java/com/chilborne/covidradar/controllers/HealthWardController.java
+++ src/main/java/com/chilborne/covidradar/controllers/HealthWardController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class HealthWardController {
